ce78a651f4b298d34706b053c7786c266a2a29c6
MdSakifHossain <sakif015sakif@gmail.com>
2026-05-31 12:18:00 +0600

    feat: add initial profile card

---
+++ src/components/my-comps/ProfileCard.tsx
@@ -0,0 +1,33 @@
+// @ts-nocheck
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
+
+const ProfileCard = ({ config }) => {
+  return (
+    <Card className="w-full border-2 shadow-lg sm:w-sm">
+      <CardHeader>
+        <img src="/my-avatar.svg" alt="avatar" className="mx-auto w-[75%]" />
+      </CardHeader>
+      <CardContent className="flex flex-col gap-1.5">
+        <h1 className="text-center text-2xl">{config?.title}</h1>
+        <p className="text-center text-muted-foreground">
+          {config?.description}
+        </p>
+      </CardContent>
+      <CardFooter className="flex flex-col items-center justify-center gap-6">
+        <div className="flex w-full items-center justify-around">
+          {config?.socials?.map((item) => (
+            <a key={item.id} href={item.url} target="_blank">
+              <img
+                src={`/icons/${item.iconName}.svg`}
+                alt="facebook"
+                className="size-7"
+              />
+            </a>
+          ))}
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
+
+export default ProfileCard
---
+++ src/constants/index.ts
@@ -0,0 +1,40 @@
+export const config = {
+    title: "MD. Sakif Hossain",
+    description: "Im a Student who is Tired and Almost Dead.",
+    socials: [
+        {
+            id: 0,
+            name: "Facebook",
+            iconName: "facebook",
+            url: "https://www.facebook.com/sakif.hossain.583",
+
+        },
+        {
+            id: 1,
+            name: "Instagram",
+            iconName: "instagram",
+            url: "https://www.instagram.com/sakifhoossain/",
+
+        },
+        {
+            id: 2,
+            name: "YouTube",
+            iconName: "youtube",
+            url: "https://m.youtube.com/@playingwithSAKIF",
+
+        },
+        {
+            id: 3,
+            name: "Github",
+            iconName: "github",
+            url: "https://github.com/MdSakifHossain",
+        },
+        {
+            id: 4,
+            name: "Bkash",
+            iconName: "bkash",
+            url: "01910711585"
+        }
+    ]
+
+}
